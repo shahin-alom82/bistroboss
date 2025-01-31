3c6fd0108cf9ec5ffc589ded8fb09d4a88719fd6
shahinalom <shahinalom3511371@gmail.com>
2025-01-31 22:07:13 +0500

    Banner Added

--- bistrobossclient/src/components/Footer.jsx
+++ bistrobossclient/src/components/Footer.jsx
@@ -1,39 +1,42 @@
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import { FaArrowRightLong } from "react-icons/fa6";
+import Container from "./Container";
 
 const Footer = () => {
       return (
             <div className="text-white">
-                  <div className="flex flex-col lg:flex-row">
-                        {/* ---------- Left Side ------------ */}
-                        <div className="bg-[#112240] lg:w-[50%] text-center py-6">
-                              <h1 className="text-xl">CONTACT US</h1>
-                              <div className="mt-2 space-y-1">
-                                    <p>123 ABS Street, Uni 21, Bangladesh</p>
-                                    <p>+88 123456789</p>
-                                    <p>Mon - Fri: 08:00 - 22:00</p>
-                                    <p>Sat - Sun: 10:00 - 23:00</p>
-                              </div>
-                        </div>
-                        {/* ----------- Right Side ----------- */}
-                        <div className=" bg-slate-700 lg:w-[50%] text-center py-6">
-                              <h1 className="text-xl uppercase">Follow US</h1>
-                              <div className="mt-2">
-                                    <p>Join us on social media</p>
-                                    <div className="flex items-center gap-4 justify-center mt-1.5">
-                                          <FaFacebookF size={20} />
-                                          <FaTwitter size={20} />
-                                          <FaInstagram size={20} />
+                  <Container>
+                        <div className="flex flex-col lg:flex-row">
+                              {/* ---------- Left Side ------------ */}
+                              <div className="bg-[#112240] lg:w-[50%] text-center py-6">
+                                    <h1 className="text-xl">CONTACT US</h1>
+                                    <div className="mt-2 space-y-1">
+                                          <p>123 ABS Street, Uni 21, Bangladesh</p>
+                                          <p>+88 123456789</p>
+                                          <p>Mon - Fri: 08:00 - 22:00</p>
+                                          <p>Sat - Sun: 10:00 - 23:00</p>
                                     </div>
-                                    {/* <div className="mx-auto w-[300px] flex items-center relative">
+                              </div>
+                              {/* ----------- Right Side ----------- */}
+                              <div className=" bg-slate-700 lg:w-[50%] text-center py-6">
+                                    <h1 className="text-xl uppercase">Follow US</h1>
+                                    <div className="mt-2">
+                                          <p>Join us on social media</p>
+                                          <div className="flex items-center gap-4 justify-center mt-1.5">
+                                                <FaFacebookF size={20} />
+                                                <FaTwitter size={20} />
+                                                <FaInstagram size={20} />
+                                          </div>
+                                          {/* <div className="mx-auto w-[300px] flex items-center relative">
                                           <input className="mt-4 rounded-full w-[300px] bg-slate-400 py-2 outline-none px-4" type="text" />
                                           <span className="mt-4 bg-orange-300 py-3 px-6 absolute right-0 rounded-full"><FaArrowRightLong/></span>
                                     </div> */}
+                                    </div>
                               </div>
                         </div>
-                  </div>
-                  {/* -------- Copy Right ---------- */}
-                  <h1 className="bg-black text-sm text-center py-2 tracking-wide">Copyright © CulinaryCloud. All rights reserved.</h1>
+                        {/* -------- Copy Right ---------- */}
+                        <h1 className="bg-black text-sm text-center py-2 tracking-wide">Copyright © CulinaryCloud. All rights reserved.</h1>
+                  </Container>
             </div>
       );
 };
--- bistrobossclient/src/components/Header.jsx
+++ bistrobossclient/src/components/Header.jsx
@@ -2,40 +2,43 @@ import { FaRegUser } from "react-icons/fa";
 import { nav } from "../contants";
 import { BsFillCartCheckFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Container from "./Container";
 
 const Header = () => {
       return (
-            <div className="border-b border-gray-300 px-6 py-4">
-                  <div className="flex items-center justify-between">
-                        {/* ---------Logo ------------ */}
-                        <Link to={"/"}>
-                              <h1 className="text-2xl font-semibold uppercase tracking-wide">Bistro boss</h1>
-                        </Link>
-                        {/* ---------- Nav ----------- */}
-                        <div className="flex items-center gap-10">
-                              {
-                                    nav.map((item, index) => (
+            <div>
+                  <Container className="w-full fixed top-0 left-0 right-0 z-10 opacity-70 text-white  bg-black">
+                        <div className="flex items-center justify-between py-5 px-7">
+                              {/* ---------Logo ------------ */}
+                              <Link to={"/"}>
+                                    <h1 className="text-2xl font-semibold uppercase tracking-wide">Bistro Boss</h1>
+                              </Link>
+                              {/* ---------- Nav ----------- */}
+                              <div className="flex items-center gap-10 text-white">
+                                    {nav.map((item, index) => (
                                           <div key={index}>
                                                 <h1 className="text-[18px] tracking-wide">{item?.title}</h1>
                                           </div>
-                                    ))
-                              }
-                        </div>
-                        {/* ------ Login Cart icons ------ */}
-                        <div className="flex items-center gap-10">
-                              <div className="flex items-center gap-2">
-                                    <span className="border border-gray-400 py-2 px-2 rounded-full text-gray-700"><FaRegUser size={25} /></span>
-                                    <div>
-                                          <h1 className="tracking-wide">Hello, Guest</h1>
-                                          <p className="text-sm tracking-wide">Login / Register</p>
+                                    ))}
+                              </div>
+                              {/* ------ Login Cart icons ------ */}
+                              <div className="flex items-center gap-10 text-white">
+                                    <div className="flex items-center gap-2">
+                                          <span className="border border-white py-2 px-2 rounded-full text-white">
+                                                <FaRegUser size={25} />
+                                          </span>
+                                          <div>
+                                                <h1 className="tracking-wide">Hello, Guest</h1>
+                                                <p className="text-sm tracking-wide">Login / Register</p>
+                                          </div>
+                                    </div>
+                                    <div className="relative ">
+                                          <BsFillCartCheckFill className="text-white" size={30} />
+                                          <span className="absolute right-1.5 text-xs bottom-[26px] text-white bg-green-600 px-1 rounded-full">0</span>
                                     </div>
                               </div>
-                              <div className="relative">
-                                    <BsFillCartCheckFill className="text-gray-700" size={30}/>
-                                    <span className="absolute right-1.5 text-xs bottom-[26px] text-white bg-green-600  px-1 rounded-full">0</span>
-                              </div>
                         </div>
-                  </div>
+                  </Container>
             </div>
       );
 };
